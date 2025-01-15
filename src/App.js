98e04ef4bfa418d12570c560ed1cd5c5423590ec
Suneel Kumar <suneelkatikala999@gmail.com>
2025-01-15 10:12:50 +0530

    Fixed router implementation and babel dependency

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { useState } from "react";
-import { BrowserRouter, Route, Switch } from "react-router-dom";
+import { HashRouter, Route, Switch } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer/Footer";
 import Header from "./components/Header/Header";
@@ -24,8 +24,8 @@ function App() {
   };
 
   return (
-    <BrowserRouter>
-      <div className="app" style={{ backgroundImage: 'url("/ques1.png")' }}>
+    <HashRouter>
+      <div className="app" style={{ backgroundImage: 'url("./ques1.png")' }}>
         <Header />
         <Switch>
           <Route path="/" exact>
@@ -50,7 +50,7 @@ function App() {
         </Switch>
       </div>
       <Footer />
-    </BrowserRouter>
+    </HashRouter>
   );
 }
 
